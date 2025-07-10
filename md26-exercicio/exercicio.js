36c46173450636a4c5fb2c0fdb5fd97801c3ae6b
Thiago Lopes Santos <108635915+ThiagoLopesSantos@users.noreply.github.com>
2025-07-10 20:07:42 -0300

    adicionando funções de multiplicação e saudação em Typescript (#1)

---
+++ md26-exercicio/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicar(3, 4));
+console.log(saudacao("Thiago"));
